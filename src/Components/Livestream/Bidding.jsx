e8ec89481fe13ab99bbfcf2f8d470c693389d5ae
Iris Huang <yilu.huang@gmail.com>
2021-10-16 16:22:55 -0700

    Added files skeleton

---
+++ src/Components/Livestream/Bidding.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+const Bidding = () => (
+  <div className="Bidding">
+    Bid
+  </div>
+);
+
+export default Bidding;
---
+++ src/Components/Livestream/Chat.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+const Chat = () => (
+  <div className="Chat">
+    Chat
+  </div>
+);
+
+export default Chat;
