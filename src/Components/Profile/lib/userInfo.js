6a1a6b4ee0f2784a50831e488004ac9ffb3cfc6a
Derek Samms <derek.samms@gmail.com>
2021-10-16 15:59:44 -0700

    organizing by divs; added material UI card code in comments

--- src/Components/Profile/lib/userInfo.js
+++ src/Components/Profile/lib/userInfo.js
@@ -1,111 +1,111 @@
-module.exports = {
-  userInfo: [
-    {
-      name: 'Oliver Squirtle Nomes',
-      about: 'I help wealthy entrepeneurs and celebrities find the best up and coming art to decorate their homes and places of businesses with that elusive sense of grandeur while maintaining a human touch.',
-      accountType: 'Connoisseur',
-      followers: 386,
-      following: 1564,
-      socialMedia: [
-        {
-          site: 'Facebook',
-          url: 'https://www.facebook.com/oliver-squirtle-nomes',
-        },
-        {
-          site: 'Instagram',
-          url: 'https://www.instagram.com/oliver-squirtle-nomes',
-        },
-      ],
-      upcomingEvents: [
-        {
-          date: '2021-10-31',
-          title: 'Exclusive VIP Shindig',
-        },
-        {
-          date: '2021-11-19',
-          title: 'Rin-Con: Fans of Paloma Event',
-        },
-      ],
+const userInfo = [
+  {
+    name: 'Oliver Squirtle Nomes',
+    about: 'I help wealthy entrepeneurs and celebrities find the best up and coming art to decorate their homes and places of businesses with that elusive sense of grandeur while maintaining a human touch.',
+    accountType: 'Connoisseur',
+    followers: 386,
+    following: 1564,
+    socialMedia: [
+      {
+        site: 'Facebook',
+        url: 'https://www.facebook.com/oliver-squirtle-nomes',
+      },
+      {
+        site: 'Instagram',
+        url: 'https://www.instagram.com/oliver-squirtle-nomes',
+      },
+    ],
+    upcomingEvents: [
+      {
+        date: '2021-10-31',
+        title: 'Exclusive VIP Shindig',
+      },
+      {
+        date: '2021-11-19',
+        title: 'Rin-Con: Fans of Paloma Event',
+      },
+    ],
 
-    },
-    {
-      name: 'Mara Sukhrab-Ali',
-      about: 'I\'m a lover of all things beatiful. Truth is beauty, and that is the truth, and it is beautiful, so I love it. You are beautiful, but that is not true, so I love you, but I also hate that I love you.',
-      accountType: 'Creator & Connoisseur',
-      followers: 14,
-      following: 157,
-      socialMedia: [
-        {
-          site: 'Facebook',
-          url: 'https://www.facebook.com/mara-sukhrab',
-        },
-        {
-          site: 'Instagram',
-          url: 'https://www.instagram.com/msa-alltheway',
-        },
-      ],
-      upcomingEvents: [
-        {
-          date: '2021-10-31',
-          title: 'Exclusive VIP Shindig',
-        },
-        {
-          date: '2021-11-19',
-          title: 'Rin-Con: Fans of Paloma Event',
-        },
-        {
-          date: '2021-12-17',
-          title: 'CulturePunk Christmas Party',
-        },
-        {
-          date: '2021-12-21',
-          title: 'CulturePunk Winter Solstice Party',
-        },
-        {
-          date: '2021-11-28',
-          title: 'CulturePunk Hannukah Party',
-        },
-        {
-          date: '2021-12-26',
-          title: 'CulturePunk Kwanzaa Party',
-        },
+  },
+  {
+    name: 'Mara Sukhrab-Ali',
+    about: 'I\'m a lover of all things beatiful. Truth is beauty, and that is the truth, and it is beautiful, so I love it. You are beautiful, but that is not true, so I love you, but I also hate that I love you.',
+    accountType: 'Creator & Connoisseur',
+    followers: 14,
+    following: 157,
+    socialMedia: [
+      {
+        site: 'Facebook',
+        url: 'https://www.facebook.com/mara-sukhrab',
+      },
+      {
+        site: 'Instagram',
+        url: 'https://www.instagram.com/msa-alltheway',
+      },
+    ],
+    upcomingEvents: [
+      {
+        date: '2021-10-31',
+        title: 'Exclusive VIP Shindig',
+      },
+      {
+        date: '2021-11-19',
+        title: 'Rin-Con: Fans of Paloma Event',
+      },
+      {
+        date: '2021-12-17',
+        title: 'CulturePunk Christmas Party',
+      },
+      {
+        date: '2021-12-21',
+        title: 'CulturePunk Winter Solstice Party',
+      },
+      {
+        date: '2021-11-28',
+        title: 'CulturePunk Hannukah Party',
+      },
+      {
+        date: '2021-12-26',
+        title: 'CulturePunk Kwanzaa Party',
+      },
 
-      ],
-    },
-    {
-      name: 'Millette Renoux',
-      about: 'My goal is to shine a light (literally and literarily) on things unseen. Where some see taboos, I see uncharted territory. Where some see controversy, I see conversation. Where some see a void, I see an empty canvas waiting for my brush. See my Saved-by-the-Bell inspired mixed-media tribute to Italian Gothic Architecture for the most recent and buzzworthy examples',
-      accountType: 'Creator',
-      followers: 256000,
-      following: 0,
-      socialMedia: [
-        {
-          site: 'Facebook',
-          url: 'https://www.facebook.com/millette-renoux',
-        },
-        {
-          site: 'Instagram',
-          url: 'https://www.instagram.com/milly-ngabout',
-        },
-        {
-          site: 'Deviant Art',
-          url: 'https://www.deviantart.com/milly',
-        },
-      ],
-      upcomingEvents: [
-        {
-          date: '2021-10-31',
-          title: 'Exclusive VIP Shindig',
-        },
-        {
-          date: '2021-11-19',
-          title: 'Rin-Con: Fans of Paloma Event',
-        },
-        {
-          date: '2070-12-31',
-          title: 'Lunar Colony Gala',
-        },
-      ],
-    },
-  ],
-};
+    ],
+  },
+  {
+    name: 'Millette Renoux',
+    about: 'My goal is to shine a light (literally and literarily) on things unseen. Where some see taboos, I see uncharted territory. Where some see controversy, I see conversation. Where some see a void, I see an empty canvas waiting for my brush. See my Saved-by-the-Bell inspired mixed-media tribute to Italian Gothic Architecture for the most recent and buzzworthy examples',
+    accountType: 'Creator',
+    followers: 256000,
+    following: 0,
+    socialMedia: [
+      {
+        site: 'Facebook',
+        url: 'https://www.facebook.com/millette-renoux',
+      },
+      {
+        site: 'Instagram',
+        url: 'https://www.instagram.com/milly-ngabout',
+      },
+      {
+        site: 'Deviant Art',
+        url: 'https://www.deviantart.com/milly',
+      },
+    ],
+    upcomingEvents: [
+      {
+        date: '2021-10-31',
+        title: 'Exclusive VIP Shindig',
+      },
+      {
+        date: '2021-11-19',
+        title: 'Rin-Con: Fans of Paloma Event',
+      },
+      {
+        date: '2070-12-31',
+        title: 'Lunar Colony Gala',
+      },
+    ],
+  },
+];
+
+export default userInfo;
