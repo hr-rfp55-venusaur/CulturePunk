f110367bddc5aa13c94db32ef27495f150e54233
Derek Samms <derek.samms@gmail.com>
2021-10-16 13:43:37 -0700

    added a lib folder with test photos for ProfilePhoto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import ContextObj from './ContextObj';
+import Profile from './Components/Profile/Profile';
 
 const App = () => (
   <div className="App">
     <ContextObj.Provider>
-      <header className="App-header">
+      {/* <header className="App-header">
         TEST
-      </header>
+      </header> */}
+      <Profile />
     </ContextObj.Provider>
   </div>
 );
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import '../../App.css';
+// import ContextObj from '../../ContextObj';
+import ProfilePhoto from './ProfilePhoto';
+import Buttons from './Buttons';
 
 const Profile = () => (
   <div className="Profile">
     Profile
+    <ProfilePhoto />
+    <Buttons />
   </div>
 );
 
