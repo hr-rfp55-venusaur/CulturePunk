20be9fb13c908fb62da675045e0595d082a7aa7c
Caleb Kim <kim.caleb10@gmail.com>
2021-10-18 16:57:17 -0700

    Added Switch to NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Link,
   NavLink,
   Route,
+  Switch,
 } from 'react-router-dom';
 import './App.css';
 import ContextObj from './ContextObj';
 import Login from './Components/Login';
 import NavBar from './Components/NavBar/NavBar';
-// import Profile from './Components/Profile/Profile';
-// import EventCalendar from './Components/Calendar/EventCalendar';
+import EventCalendar from './Components/Calendar/EventCalendar';
+import Livestream from './Components/Livestream/Livestream';
 import './Components/Calendar/calendar.css';
-// import Profile from './Components/Profile/Profile';
+import Profile from './Components/Profile/Profile';
 import Home from './Components/Home/Home';
 
 const App = () => (
@@ -22,9 +22,15 @@ const App = () => (
         TEST
       </header> */}
       <Router>
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/navbar" component={NavBar} />
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/events" component={EventCalendar} />
+          <Route exact path="/livestream" component={Livestream} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/navbar" component={NavBar} />
+        </Switch>
       </Router>
       {/* <EventCalendar /> */}
       {/* <Home /> */}
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,17 +5,45 @@ import {
   Link,
   NavLink,
 } from 'react-router-dom';
-import Login from '../Login';
+import './NavBar.css';
 
 const NavBar = () => {
   console.log('hello');
 
   return (
-    <div>
-      Nav Bar here
-      <Link to="/login">
-        <Login />
-      </Link>
+    <div className="NavContainer">
+      <ul>
+        <li>
+          <Link to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/market">
+            Market
+          </Link>
+        </li>
+        <li>
+          <Link to="/events">
+            Events
+          </Link>
+        </li>
+        <li>
+          <Link to="/livestream">
+            Livestream
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile">
+            Profile
+          </Link>
+        </li>
+        <li>
+          <Link to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
