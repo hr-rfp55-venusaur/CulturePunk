3cefa276974df72f6d2053a921475eb634711de4
Dora Jiang <nannan@Erics-MacBook-Pro.local>
2021-10-19 16:39:16 -0700

    signup complete

--- src/Components/Login/Signup.jsx
+++ src/Components/Login/Signup.jsx
@@ -1,14 +1,17 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Grid, Paper, TextField, Button, Link } from '@material-ui/core';
 
 import '../../App.css';
 
-import {useAuth} from './contexts/AuthContext';
+import {useAppContext} from '../../ContextObj'; //use Auth context
 
 const Signup = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup, currentUser } = useAppContext();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const paperStyle = {
     padding: 20,
@@ -21,18 +24,43 @@ const Signup = () => {
     marginBottom: '2vh'
   };
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(`1 --- emailref ${emailRef.current.value} passwordref ${passwordRef.current.value} passwordConfirmRef ${passwordConfirmRef}`);
+    // form validation
+    if(passwordRef.current.value !== passwordConfirmRef.current.value) {
+      alert('Passwords do not match.');
+      return setError('Passwords do not match.');
+    }
+
+    // sign up a new user
+    try {
+      setError('');
+      setLoading(true);
+      console.log(`3 -- ${emailRef.current.value} ${passwordRef.current.value}`);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch(error) {
+      console.log(`4 -- error ${error}` );
+      setError('Failed to create a new account.');
+    }
+    setLoading(false);
+  };
+
+  console.log(`2 --- current user ${currentUser}`);
+
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
         <Grid align='center'>
           <h2>Sign Up</h2>
         </Grid>
-        <TextField label='Email' variant='filled' style={textFieldStyle} ref={emailRef} fullWidth required/>      
-        <TextField label='Password'  variant='filled' style={textFieldStyle} ref={passwordRef} fullWidth required/>
-        <TextField label='Re-enter Password' variant='filled' style={textFieldStyle} ref={passwordConfirmRef} fullWidth required/>
-        <Grid>
-          <Button type='submit' color='primary' variant='contained' xs={12} >Sign UP</Button> 
-        </Grid>
+        <form onSubmit={handleSubmit}>
+          <TextField label='Email' variant='filled' style={textFieldStyle} inputRef={emailRef} fullWidth={true} required/>      
+          <TextField type='password' label='Password'  variant='filled' style={textFieldStyle} inputRef={passwordRef} fullWidth={true} required/>
+          <TextField type='password' label='Re-enter Password' variant='filled' style={textFieldStyle} inputRef={passwordConfirmRef} fullWidth={true} required/>
+          <Button disabled={loading} type='submit' color='primary' variant='contained' xs={12} >Sign UP</Button> 
+        </form>
+        
         <Grid style={{marginTop: '1vh'}}>
           Already have an account? <Link href='#'>Sign in</Link>
         </Grid>
