5f0bc7450548c759a79da0dfa1460bf9042a5e75
John Fa <john6fa@gmail.com>
2021-10-19 11:10:31 -0700

    app comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import './App.css';
 import ContextObj from './ContextObj';
 // import Profile from './Components/Profile/Profile';
 import EventCalendar from './Components/Calendar/EventCalendar';
-import './Components/Calendar/calendar.css';
 // import Profile from './Components/Profile/Profile';
 // import Home from './Components/Home/Home';
 
 const App = () => (
   <div className="App">
     <ContextObj.Provider>
-      {/* <header className="App-header">
-        TEST
-      </header> */}
       <EventCalendar />
       {/* <Home /> */}
     </ContextObj.Provider>
