348143e25d3763d485572a01a51f1a3a1ee7d82d
Dora Jiang <nannan@Erics-MacBook-Pro.local>
2021-10-20 09:37:46 -0700

    clean up code remove console log

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Redirect, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Grid, Paper, Avatar, TextField, Checkbox, FormControlLabel, Button, Link, Typography } from '@material-ui/core';
 
 import '../../App.css';
