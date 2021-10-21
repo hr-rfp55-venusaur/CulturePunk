7b6dcb087d0588b1a84d672261c3f8756a99ae34
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-10-21 14:49:11 -0700

    CSS tweaks

--- src/Components/Market/Sort.jsx
+++ src/Components/Market/Sort.jsx
@@ -5,36 +5,37 @@ import Fab from '@mui/material/Fab';
 import SplitButton from './SplitButton';
 
 export default function Sort({ setSortValue, setDirection }) {
-  const captureSortValue = function (e) {
-    setSortValue(e.target.value);
-  };
+  const captureSortValue = (e) => setSortValue(e.target.value);
 
   return (
-    <div className="sidebar">
-      <h2>Sort By</h2>
-      <Stack direction="column" spacing={2}>
-        <Fab color="secondary" variant="extended" size="medium" onClick={captureSortValue} value="sale_date" sx={{fontWeight: 'bold' }}>
-          Last Sale
-        </Fab>
+    <div className="market-sidebar">
+      {/* <h1>. Culture Punk . </h1>
+      <h2> NFT Gallery </h2> */}
+      <div className="market-sidebar-innner">
+        <h3>Sort By</h3>
+        <Stack direction="column" spacing={2}>
+          <Fab color="secondary" variant="extended" size="medium" onClick={captureSortValue} value="sale_date" sx={{ fontWeight: 'bold' }}>
+            Last Sale
+          </Fab>
 
-        <Fab color="secondary" variant="extended" size="medium" onClick={captureSortValue} value="sale_count" sx={{fontWeight: 'bold' }}>
-          Number of Sales
-        </Fab>
+          <Fab color="secondary" variant="extended" size="medium" onClick={captureSortValue} value="sale_count" sx={{ fontWeight: 'bold' }}>
+            Number of Sales
+          </Fab>
 
-        <Fab color="secondary" variant="extended" size="medium" onClick={captureSortValue} value="pk" sx={{fontWeight: 'bold' }}>
-          NFT Token ID
-        </Fab>
-      </Stack>
-
-      <br />
-
-      <SplitButton setDirection={setDirection} />
+          <Fab color="secondary" variant="extended" size="medium" onClick={captureSortValue} value="pk" sx={{ fontWeight: 'bold' }}>
+            NFT Token ID
+          </Fab>
+        </Stack>
+        <br />
+        <SplitButton setDirection={setDirection} />
+      </div>
     </div>
   );
 }
 
 Sort.propTypes = {
   setSortValue: PropTypes.func.isRequired,
+  setDirection: PropTypes.func.isRequired,
 };
 
 /*
