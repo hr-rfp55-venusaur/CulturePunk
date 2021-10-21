9ea7db3617b89a0c990400155f8f7e8e30b95f86
Derek Samms <derek.samms@gmail.com>
2021-10-21 16:31:44 -0700

    added descriptions to the items in userInfo

--- src/data/userInfo.js
+++ src/data/userInfo.js
@@ -73,34 +73,40 @@ const userInfo = [
         title: 'Heatwave',
         subtitle: 'Paloma Rincón',
         artistId: 0,
+        description: 'Oh yes! It is again this time of the year.',
         image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ftrendland.com%2Fwp-content%2Fuploads%2F2015%2F12%2Fpaloma_Rincón_heatwaves_6.jpg&f=1&nofb=1',
       },
       {
         title: 'Life & Death',
         subtitle: 'Paloma Rincón',
+        description: 'a very special artwork created for the Time to Rise drop',
         artistId: 0,
         image: 'https://palomarincon.com/wp-content/uploads/2021/09/Life-Dead-low.gif',
       },
       {
         title: 'Mexican Feast',
         subtitle: 'Paloma Rincón',
+        description: 'A celebration of colour, traditional mexican handcrafts and gastronomy.',
         artistId: 0,
         image: 'https://palomarincon.com/wp-content/uploads/2020/10/mexican-feast-pink-meal_01-1024x768.jpg',
       },
       {
         title: 'OFFF Synesthesia Touch',
         subtitle: 'Paloma Rincón',
+        description: 'My campaign for @offfbogota',
         artistId: 0,
         image: 'https://palomarincon.com/wp-content/uploads/2020/03/touch_3000px-1-scaled.jpg',
       },
       {
         title: 'OFFF Synesthesia Smell',
         subtitle: 'Paloma Rincón',
+        description: 'an exploration on the 5 senses through textures, colours and materials',
         artistId: 0,
         image: 'https://palomarincon.com/wp-content/uploads/2020/03/smell_3000px-1024x1024.jpg',
       },
       {
         title: 'Mental Health Banana',
+        description: 'Have you ever felt this way?',
         subtitle: 'Paloma Rincón',
         artistId: 0,
         image: 'https://palomarincon.com/wp-content/uploads/2020/10/mental-health-banana_3000px-1024x1024.jpg',
@@ -108,6 +114,7 @@ const userInfo = [
       {
         title: 'Discomelon',
         subtitle: 'Paloma Rincón',
+        description: 'Oh yes! It is again this time of the year.',
         artistId: 0,
         image: 'https://palomarincon.com/wp-content/uploads/2020/04/Dicscomelon-3000px-scaled.jpg',
       },
@@ -250,6 +257,7 @@ const userInfo = [
         actualArtist: 'unknown',
         subtitle: 'Mara Sukhrab-Ali',
         artistId: 2,
+        description: 'Riding the Bull Market',
         image:
           'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcvj.ch%2Fwp-content%2Fuploads%2F2020%2F12%2Fbeeple-bitcoin-bull.jpg&f=1&nofb=1',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -259,6 +267,7 @@ const userInfo = [
         actualArtist: 'FuturePunk',
         subtitle: 'Millette Renoux',
         artistId: 3,
+        description: 'Just like all of us.',
         image:
           'https://cdn.substack.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F31c42cf0-d2d6-49f0-aa4e-181a613bde5c_1018x1018.png',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -267,6 +276,7 @@ const userInfo = [
         title: 'Warmer Than Gold',
         actualArtist: 'FuturePunk',
         subtitle: 'Mara Sukhrab-Ali',
+        description: 'Just like all of us.',
         artistId: 2,
         image:
           'https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F5c349750-2862-4cf3-bd7c-66dc91b7dbb4_1170x900.png',
@@ -276,6 +286,7 @@ const userInfo = [
         title: 'Heatwave Pine Saver',
         actualArtist: 'Paloma Rincón',
         subtitle: 'Paloma Rincón',
+        description: 'Bye bye Heat Wave, welcome holiday.',
         artistId: 0,
         image:
           'https://palomarincon.com/wp-content/uploads/2020/04/HeatWave-Pine-Saver-3000px-2048x2048.jpg',
@@ -286,6 +297,7 @@ const userInfo = [
         actualArtist: 'Asleep Hernandez',
         subtitle: 'Millette Renoux',
         artistId: 3,
+        description: '(Toad)',
         image:
           'https://images.squarespace-cdn.com/content/v1/600b910d70305b6eb07d9c39/1611508868436-UXT6O7G9S3K2QUGAXZRT/tumblr_inline_numqrlszSM1tas015_500.jpg?format=750w',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -368,6 +380,7 @@ const userInfo = [
         title: 'Warmer Than Gold',
         actualArtist: 'FuturePunk',
         subtitle: 'Mara Sukhrab-Ali',
+        description: 'Just like all of us.',
         artistId: 2,
         image:
           'https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F5c349750-2862-4cf3-bd7c-66dc91b7dbb4_1170x900.png',
@@ -378,6 +391,7 @@ const userInfo = [
         actualArtist: 'unknown',
         subtitle: 'Mara Sukhrab-Ali',
         artistId: 2,
+        description: 'Riding the Bull Market',
         image:
           'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcvj.ch%2Fwp-content%2Fuploads%2F2020%2F12%2Fbeeple-bitcoin-bull.jpg&f=1&nofb=1',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -387,6 +401,7 @@ const userInfo = [
         actualArtist: 'FuturePunk',
         subtitle: 'Mara Sukhrab-Ali',
         artistId: 2,
+        description: 'Strong enough to be fragile.',
         image:
           'https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F0d6e10fb-80fe-449f-84a6-d972fa9797ba_1358x1284.png',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -396,6 +411,7 @@ const userInfo = [
         actualArtist: 'Asleep Hernandez',
         subtitle: 'Mara Sukhrab-Ali',
         artistId: 2,
+        description: 'Strong enough to be fragile.',
         image:
           'https://images.squarespace-cdn.com/content/v1/600b910d70305b6eb07d9c39/1611508871233-37B9AFIBEI43YAZ0OI8N/tumblr_inline_q0if9078ah1tas015_500.jpg?format=750w',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -407,6 +423,7 @@ const userInfo = [
         actualArtist: 'FuturePunk',
         subtitle: 'Mara Sukhrab-Ali',
         artistId: 2,
+        description: 'Strong enough to be fragile.',
         image:
           'https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F0d6e10fb-80fe-449f-84a6-d972fa9797ba_1358x1284.png',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -416,6 +433,7 @@ const userInfo = [
         actualArtist: 'Lasergun Factory',
         subtitle: 'Millette Renoux',
         artistId: 3,
+        description: 'laser head vision',
         image:
           'https://491569-1551798-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2021/04/BLUE_can.gif',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -425,6 +443,7 @@ const userInfo = [
         actualArtist: 'Lasergun Factory',
         subtitle: 'Millette Renoux',
         artistId: 3,
+        description: 'laser vision head',
         image:
           'https://491569-1551798-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2021/04/OTHER_can-1.png',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -434,6 +453,7 @@ const userInfo = [
         actualArtist: 'Paloma Rincón',
         subtitle: 'Paloma Rincón',
         artistId: 0,
+        description: 'Bye bye Heat Wave, welcome holiday.',
         image:
           'https://palomarincon.com/wp-content/uploads/2020/04/HeatWave-Pine-Saver-3000px-2048x2048.jpg',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -443,6 +463,7 @@ const userInfo = [
         actualArtist: 'Asleep Hernandez',
         subtitle: 'Mara Sukhrab-Ali',
         artistId: 2,
+        description: 'Strong enough to be fragile.',
         image:
           'https://images.squarespace-cdn.com/content/v1/600b910d70305b6eb07d9c39/1611508871233-37B9AFIBEI43YAZ0OI8N/tumblr_inline_q0if9078ah1tas015_500.jpg?format=750w',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -499,6 +520,7 @@ const userInfo = [
         actualArtist: 'Lasergun Factory',
         subtitle: 'Millette Renoux',
         artistId: 3,
+        description: 'laser head vision',
         image:
           'https://491569-1551798-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2021/04/BLUE_can.gif',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
@@ -508,6 +530,7 @@ const userInfo = [
         actualArtist: 'Lasergun Factory',
         subtitle: 'Millette Renoux',
         artistId: 3,
+        description: 'laser vision head',
         image:
           'https://491569-1551798-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2021/04/OTHER_can-1.png',
         bkg: 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=20&m=1040250650&s=612x612&w=0&h=lEWpioJ3jet0QIZVBoU2Ygaua8YMHFfHN1mvT28xRZ4=',
