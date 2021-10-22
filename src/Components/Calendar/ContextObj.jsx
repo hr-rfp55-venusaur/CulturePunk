7187c832f55a89f329ddf74fced1513346c51898
John Fa <john6fa@gmail.com>
2021-10-21 21:43:49 -0700

    test work

---
+++ src/Components/Calendar/ContextObj.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const ContextObj = React.createContext({});
+
+// eslint-disable-next-line import/prefer-default-export
+export { ContextObj };
--- src/Components/Calendar/calendar.test.jsx
+++ src/Components/Calendar/calendar.test.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-// eslint-disable-next-line import/no-unresolved
 import '@testing-library/jest-dom';
 import {
   render, describe, test,
 } from '@testing-library/react';
 
+import { ContextObj } from './ContextObj';
 import App from '../../App';
 
 describe('App', () => {
   test('renders App component', () => {
-    render(<App />);
+    render(<ContextObj.Provider><App /></ContextObj.Provider>);
   });
 });
