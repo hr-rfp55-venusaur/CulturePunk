0413018e9ec561936eda86f4f8d4eb71a34b1940
Caleb Kim <kim.caleb10@gmail.com>
2021-10-22 11:46:40 -0700

    CSS Styling for Navigation bar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,36 +4,38 @@ import {
 } from 'react-router-dom';
 import './NavBar.css';
 
+const none = { textDecoration: 'none', color: 'black' };
+
 const NavBar = () => (
   <div className="NavContainer">
     <ul>
       <li>
-        <Link to="/">
+        <Link to="/" style={none}>
           Home
         </Link>
       </li>
       <li>
-        <Link to="/market">
+        <Link to="/market" style={none}>
           Market
         </Link>
       </li>
       <li>
-        <Link to="/events">
+        <Link to="/events" style={none}>
           Events
         </Link>
       </li>
       <li>
-        <Link to="/livestream">
+        <Link to="/livestream" style={none}>
           Livestream
         </Link>
       </li>
       <li>
-        <Link to="/profile">
+        <Link to="/profile" style={none}>
           Profile
         </Link>
       </li>
       <li>
-        <Link to="/login">
+        <Link to="/login" style={none}>
           Login
         </Link>
       </li>
