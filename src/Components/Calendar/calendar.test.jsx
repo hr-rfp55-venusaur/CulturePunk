3000d06be84fb578db2a604dfefe89bbcd842a02
John Fa <john6fa@gmail.com>
2021-10-21 18:23:03 -0700

    test

--- src/Components/Calendar/calendar.test.jsx
+++ src/Components/Calendar/calendar.test.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-// import '@testing-library/jest-dom';
+// eslint-disable-next-line import/no-unresolved
+import '@testing-library/jest-dom';
 import {
-  render, describe, test, cleanup, afterEach,
+  render, describe, test,
 } from '@testing-library/react';
 
 import App from '../../App';
 
 describe('App', () => {
-  afterEach(cleanup);
-
   test('renders App component', () => {
     render(<App />);
   });
